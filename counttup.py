0d7c05a9ff3b89fac9799f3f3fe5e3337a1143b5
arpang876-creator <arpang876@gmail.com>
2026-06-20 02:45:24 +0530

    Add files via upload

---
+++ counttup.py
@@ -0,0 +1,6 @@
+tup=("c", "a", "b", "c", "d", "c","b","c","a")
+print(tup.count("c"))
+
+tup=list(tup)
+tup.sort()
+print(tup)
---
+++ storinginlist.py
@@ -0,0 +1,8 @@
+a=input("Enter your first fav movie:")
+b=input("Enter your second fav movie:")
+c=input("Enter your third fav movie:")
+list=[]
+list.append(a)
+list.append(b)
+list.append(c)
+print(list)
