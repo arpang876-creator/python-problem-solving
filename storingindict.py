f6f88d94eec948b1191dbc128d8d6da1f8fe57b0
arpang876-creator <arpang876@gmail.com>
2026-06-20 21:15:47 +0530

    Add files via upload

---
+++ storingindict.py
@@ -0,0 +1,7 @@
+dict={}
+x=input("Enter the marks")
+y=input("Enter the marks")
+z=input("Enter the marks")
+
+dict.update({"English": x, "Maths": y, "Science": z})
+print(dict)
---
+++ union&intersectionsets.py
@@ -0,0 +1,7 @@
+set1={22,33,44}
+set2={44,55,66}
+set3={44,77,88,99}
+print(set1.union(set2))
+print(set1.intersection(set2))
+print(set1.union(set2).union(set3))
+print(set1.intersection(set2).intersection(set3))
