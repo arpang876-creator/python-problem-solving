05bb0a3fbb9b4cb47792f86262b85e8d982c083a
arpang876-creator <arpang876@gmail.com>
2026-06-20 15:05:08 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,14 @@
+'''
+collection={1,2,3,4,"arpan","arnav"}
+
+print(collection)
+print(type(collection))
+'''
+
+#set is a collection of unordered and unindexed elements. It does not allow duplicate values.
+
+collection={1,2,3,4,"arpan","arnav",1,2,3}
+print(collection)# this will not give error but it will avoid the duplicate values and print only unique values
+print(len(collection))# will avoid the duplicate values and print only length of unique values
+
+
