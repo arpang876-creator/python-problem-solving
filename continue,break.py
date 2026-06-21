f247370b0206aeca4aa6035a244529c78b23427a
arpang876-creator <arpang876@gmail.com>
2026-06-21 23:54:04 +0530

    Add files via upload

---
+++ continue,break.py
@@ -0,0 +1,23 @@
+#print even numbers from 1 to 10 from continue
+
+'''i=1
+while i <= 10:
+    if(i % 2 != 0):
+        i += 1
+        continue
+    print(i)
+    i += 1
+    '''
+
+
+
+#break
+x=6
+i=1
+while i<=10:
+    print(i)
+    i+=1
+    if(i==x):
+        print(i)
+        break
+    i+=1
