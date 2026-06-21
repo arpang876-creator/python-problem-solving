6d5e286ab02160d4dbe6148c9a2b57783bdbc570
arpang876-creator <arpang876@gmail.com>
2026-06-21 12:20:16 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,31 @@
+'''i=1
+while i<=20:
+    print("hello arpan")
+    i=i+1'''
+
+#print number from 1 to 100
+'''i=1
+while i<=100:
+    print(i)
+    i+=1'''
+
+#print number from 100 to 1
+'''i=100
+while i>=1:
+    print(i)
+    i-=1'''
+
+#print a multiplication table of number n
+'''
+n = int(input("enter any number"))
+j = 1
+while j <= 10:
+    print(f"{n} x {j} = {n * j}")
+    j += 1
+    '''
+    
+
+j=1
+while j<=10:
+    print(j*j)
+    j+=1
