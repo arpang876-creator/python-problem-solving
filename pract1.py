384a6f834dc003c294939c9f60ab12a2b4f17c92
Arpan Ghosh <arpang876@gmail.com>
2026-06-30 11:50:59 +0530

    Add files via upload

---
+++ pract1.py
@@ -0,0 +1,8 @@
+#prime number or not
+
+n=int(input("Enter any number"))
+
+if(n%2!=0):
+    print("It is a prime number")
+else:
+    print("It is not an prime number")
---
+++ pract2.py
@@ -0,0 +1,7 @@
+#odd and even numbers
+n=int(input("Enter any number"))
+
+if n % 2 == 0:
+    print("even number")
+else:
+    print("odd number")
