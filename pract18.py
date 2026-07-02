fbdcef5b290eccb67033230ed43d0d3368718196
Arpan Ghosh <arpang876@gmail.com>
2026-07-02 12:06:52 +0530

    Add files via upload

---
+++ pract18.py
@@ -0,0 +1,5 @@
+#print even numbers from 1 to n
+n=int(input("Enter any number:"))
+for i in range(2,n+1,2):
+   print(i)
+   
---
+++ pract19.py
@@ -0,0 +1,4 @@
+n=int(input("Enter any number"))
+for i in range(1,11):
+    table=n*i
+    print(f"{n} x {i} = {table}")
