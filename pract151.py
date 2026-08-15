e710553c48f86e45c6e7473d6199692daa5fce88
Arpan Ghosh <arpang876@gmail.com>
2026-08-15 11:00:27 +0530

    Add files via upload

---
+++ pract151.py
@@ -0,0 +1,15 @@
+try:
+    a = int(input("Enter a number"))
+    b = int(input("Enter a number"))
+    c = int(input("Enter a number"))
+    d = int(input("Enter a number")) 
+    e = int(input("Enter a number"))
+    sum = a + b + c + d + e
+    print("Total Sum : ",sum)
+
+except:
+    print("invalid input , skipped")
+    
+
+
+    
