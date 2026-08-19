3aa423446f0a8ce95326b82ddee8667e9c41f265
Arpan Ghosh <arpang876@gmail.com>
2026-08-19 21:40:40 +0530

    Add files via upload

---
+++ pract155.py
@@ -0,0 +1,17 @@
+students = {
+    "student1": {
+        "Name": "Arpan",
+        "Age": 20,
+        "Marks": 95
+    },
+    "student2": {
+        "Name": "Rahul",
+        "Age": 21,
+        "Marks": 78
+    }
+}
+
+print(students["student1"]["Name"])
+print(students["student1"]["Age"])
+print(students["student2"]["Name"])
+print(students["student2"]["Age"])
