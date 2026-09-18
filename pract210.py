f8899ff9c82cc21ab6948912d54e251598886dfb
Arpan Ghosh <arpang876@gmail.com>
2026-09-18 09:19:55 +0530

    Add files via upload

---
+++ pract210.py
@@ -0,0 +1,4 @@
+import numpy as np
+array = np.array([50, 10, 40, 20, 30])
+
+print(np.sort(array))
---
+++ pract211.py
@@ -0,0 +1,6 @@
+import numpy as np
+array = np.array([25, 80, 45, 100, 60])
+
+print(np.argmax(array))
+
+print(np.argmin(array))
