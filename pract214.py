0561d7023c270369fbc46b02533e701391a42b9f
Arpan Ghosh <arpang876@gmail.com>
2026-09-20 12:51:40 +0530

    Add files via upload

---
+++ pract214.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+array=np.random.randint(1, 100,size = 5)
+print(array)
+
+print(np.random.randint(1,100,size = [ 3 , 3]))
---
+++ pract215.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+array = np.random.randint(1,101, size = 10)
+
+print(array)
+print(array[array > 50])
